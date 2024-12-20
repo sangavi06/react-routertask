c0d85fdd5efd41ab5e06f2740f28de82e7e35ca6
sangavi06 <sangavikavi06@gmail.com>
2024-12-20 15:51:51 +0530

    commited changes

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+
+function Footer() {
+  return (
+    <>
+    <footer className="py-5 bg-dark">
+        <div className="container">
+            <p className="m-0 text-center text-white">Copyright <Link to={"mailTo:vijaymayhul@gmail.com"}>©vijaymayhul@gmail.com</Link> 2024</p>
+        </div>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
